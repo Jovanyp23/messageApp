ceb94a025d836a4d8ade61d9b6b8e53013e8ba4c
Jovany <jovanypena@csus.edu>
2021-07-16 10:02:18 -0700

    encoding our input

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
+
+    const input = document.querySelector('#message-input');
+
+    const coder = btoa(input.value);
+    console.log(coder);
+
+    document.querySelector('#link-input').value = coder; 
+
+
 });
