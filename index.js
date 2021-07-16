6b1e4291129d144949c995e9f4555159046b6a31
Jovany <jovanypena@csus.edu>
2021-07-16 11:21:06 -0700

    displaying decoded message

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+const { hash } =window.location; 
+
+const message =atob(hash.replace('#',''));
+if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message; 
+}
+
 
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
+    document.querySelector("#message-form").classList.add('hide');
+    document.querySelector("#link-form").classList.remove('hide');
+
     const input = document.querySelector('#message-input');
 
     const coder = btoa(input.value);
-    console.log(coder);
+   // console.log(coder);
 
-    document.querySelector('#link-input').value = coder; 
+    const mess = document.querySelector('#link-input')
+    mess.value = `${window.location}#${coder}`; 
+    mess.select();
 
 
 });
